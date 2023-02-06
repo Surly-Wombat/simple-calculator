c623f1aa182920cab25e9579af9bac53a10e65e7
Surly-Wombat <113541817+Surly-Wombat@users.noreply.github.com>
2023-02-06 14:59:58 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,99 @@
+let numString = "0";
+let buffer = 0;
+let lastOperation = "";
+
+const screen = document.querySelector(".screen");
+
+function addNum(num) {
+    if(numString != "0") {
+        numString += num;
+    }
+    else if(num != "0") {
+        numString = num;
+    }
+    screen.innerHTML = numString;
+}
+
+function special(char) {
+    if(char == "C") {
+        numString = "0";
+    }
+    else if(char == "←") {
+        if(numString.length > 1) {
+            numString = numString.substring(0,numString.length-1);
+            if(numString == "-") {
+                numString = "0";
+            }
+        }
+        else {
+            numString = "0"
+        }
+    }
+    screen.innerHTML = numString;
+}
+
+function operation(char) {
+    if(char != "=") {
+        if(!(lastOperation == "" || buffer == 0)) {
+            calculate()
+        }
+        lastOperation = char;
+        buffer = parseInt(numString)
+        numString = "0";
+    }
+    else {
+        if(lastOperation == "") {
+            return;
+        }
+        calculate();
+        lastOperation = "";
+        buffer = 0;
+    }
+}
+
+function calculate() {
+    let currentNum = parseInt(numString);
+    switch(lastOperation) {
+        case "+":
+            currentNum = buffer + currentNum;
+            break;
+        case "−":
+            currentNum = buffer - currentNum;
+            break;
+        case "×":
+            currentNum = buffer * currentNum;
+            break;
+        case "÷":
+            if(currentNum == 0) {
+                currentNum = NaN;
+                break;
+            }
+            currentNum = buffer / currentNum;
+            break;
+    }
+    numString = currentNum.toString();
+    buffer = 0;
+    lastOperation = "";
+    screen.innerHTML = numString;
+}
+
+const nums = document.querySelectorAll(".num");
+nums.forEach(num => {
+    num.addEventListener("click",function() {
+        addNum(this.innerHTML);
+    })
+});
+
+const specials = document.querySelectorAll(".special");
+specials.forEach(specialElement => {
+    specialElement.addEventListener("click",function() {
+        special(this.innerHTML);
+    })
+});
+
+const operations = document.querySelectorAll(".operation");
+operations.forEach(operationElement => {
+    operationElement.addEventListener("click",function() {
+        operation(this.innerHTML);
+    })
+});
